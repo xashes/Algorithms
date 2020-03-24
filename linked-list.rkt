349be09141d798a76b75d752be0551096d238cd3
Cantong <cantong@Cantongs-Mini.lan>
2020-03-24 23:51:11 +0800

    ll-append

--- linked-list.rkt
+++ linked-list.rkt
@@ -11,10 +11,11 @@
 
 ;; API
 ;; construct - cons
-;; item - car
-;; pointer - cdr
+;; ll-first - car
+;; ll-rest - cdr
 
 ;; Linked-List ::= '() | (cons any/c Linked-List)
+;; define type
 (struct node (item pointer) #:transparent)
 (define/contract (llist? v)
   (-> any/c boolean?)
@@ -24,4 +25,48 @@
 (module+ test
   (check-true (llist? '()))
   (check-true (llist? (node 'a '())))
+  )
+
+;; constructor
+(define/contract (construct v llst)
+  (-> any/c llist? llist?)
+  (node v llst)
+  )
+(module+ test
+  (check-equal? (construct 'a '()) (node 'a '()))
+  (check-equal? (construct 'a (construct 'b '()))
+                (node 'a (node 'b '())))
+  )
+
+;; extractor
+(define/contract (ll-first nd)
+  (-> node? any/c)
+  (node-item nd)
+  )
+(module+ test
+  (check-equal? (ll-first (node 'a '())) 'a)
+  )
+
+(define/contract (ll-rest nd)
+  (-> node? llist?)
+  (node-pointer nd)
+  )
+(module+ test
+  (check-equal? (ll-rest (node 'a '())) '())
+  )
+
+;; ll-append: append an item to the end of a llist
+;; O(n)
+(define/contract (ll-append llst v)
+  (-> llist? any/c llist?)
+  (if (null? llst)
+      (node v '())
+      (construct (ll-first llst)
+                 (ll-append (ll-rest llst) v)))
+  )
+(module+ test
+  (check-equal? (ll-append '() 'a) (node 'a '()))
+  (check-equal? (ll-append (node 'b '()) 'a) (node 'b (node 'a '())))
+  (check-equal? (ll-append (node 'a (node 'b '())) 'c)
+                (node 'a (node 'b (node 'c '()))))
   )
